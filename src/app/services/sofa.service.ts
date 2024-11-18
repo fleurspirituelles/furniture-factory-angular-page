c9e87f19ae7683707d3882c3efabbbe02324577e
talitha <talitha.vieira@aluno.ifsp.edu.br>
2024-11-18 18:46:24 -0300

    feat: refactor sofa service to use sofa class for instance creation

--- src/app/services/sofa.service.ts
+++ src/app/services/sofa.service.ts
@@ -1,17 +1,15 @@
-// src/app/services/sofa.service.ts
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class SofaService {
-  style: string = '';
-  material: string = '';
-  seats: number = 0;
-  color: string = '';
-  length: number = 0;
-  width: number = 0;
-  weight: number = 0;
+export class Sofa {
+  constructor(
+    public style: string,
+    public material: string,
+    public seats: number,
+    public color: string,
+    public length: number,
+    public width: number,
+    public weight: number
+  ) { }
 
   display(): string {
     return `This luxurious sofa embraces the ${this.style} style, upholstered in fine ${this.material}, comfortably accommodating up to ${this.seats} guests.`;
@@ -23,5 +21,22 @@ export class SofaService {
 
   isHeavy(threshold: number): boolean {
     return this.weight > threshold;
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SofaService {
+  createSofa(
+    style: string,
+    material: string,
+    seats: number,
+    color: string,
+    length: number,
+    width: number,
+    weight: number
+  ): Sofa {
+    return new Sofa(style, material, seats, color, length, width, weight);
   }
 }
