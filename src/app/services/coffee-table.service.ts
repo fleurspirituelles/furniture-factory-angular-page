b5a94c9b2133a0d67e22bae9aeb78962f9eb1a53
talitha <talitha.vieira@aluno.ifsp.edu.br>
2024-11-18 18:46:12 -0300

    feat: refactor coffee table service to use class for instance creation

--- src/app/services/coffee-table.service.ts
+++ src/app/services/coffee-table.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class CoffeeTableService {
-  style: string = '';
-  material: string = '';
-  shape: string = '';
-  color: string = '';
-  diameter: number = 0;
-  weight: number = 0;
+export class CoffeeTable {
+  constructor(
+    public style: string,
+    public material: string,
+    public shape: string,
+    public color: string,
+    public diameter: number,
+    public weight: number
+  ) { }
 
   display(): string {
     return `This coffee table showcases a timeless ${this.shape} design in the ${this.style} style, meticulously crafted from ${this.material}.`;
@@ -21,5 +20,21 @@ export class CoffeeTableService {
 
   isEasyToMove(threshold: number): boolean {
     return this.weight < threshold;
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoffeeTableService {
+  createCoffeeTable(
+    style: string,
+    material: string,
+    shape: string,
+    color: string,
+    diameter: number,
+    weight: number
+  ): CoffeeTable {
+    return new CoffeeTable(style, material, shape, color, diameter, weight);
   }
 }
